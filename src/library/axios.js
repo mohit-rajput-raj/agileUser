af9160b7f544f68d96ec3a43866af8d3a3fb3167
mohit-rajput-raj <rajputmohit19101169@gmail.com>
2026-06-19 11:38:58 +0530

    f

--- src/library/axios.js
+++ src/library/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosApi = axios.create({
-  baseURL: process.env.DEVELOPMENT_MODE === "true" ? process.env.BACKEND_URL_LOCAL : process.env.BACKEND_URL_PRODUCTION,
+  baseURL: import.meta.env.MODE === "development" ? process.env.BACKEND_URL_LOCAL : process.env.BACKEND_URL_PRODUCTION,
   withCredentials: true,
 });
